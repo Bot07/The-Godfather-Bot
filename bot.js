d37024384f47bf07671f6805c76c4100cd120718
domates06 <35399633+Bot07@users.noreply.github.com>
2018-07-24 14:08:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,11 +4,11 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-var prefix = 'f!'
+var prefix = 'tgf!'
 
 
 
-client.on("ready", () => {client.user.setGame('f!yardım | Eski Sürümü Yükledim Aktif Olacak Ama Güncelleme Olmayacak | Sunucu Sayısı: ' + client.guilds.size , 'https://twitch.tv/TheGodFather')
+client.on("ready", () => {client.user.setGame('tgf!yardım | Eski Sürümü Yükledim Aktif Olacak Ama Güncelleme Olmayacak | Sunucu Sayısı: ' + client.guilds.size , 'https://twitch.tv/TheGodFather')
 
 });
 
